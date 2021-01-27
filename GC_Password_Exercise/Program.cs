5e8683a9a58e0f59efa7eaf917b16e66522a4968
antonio <antoniomanzari@gmail.com>
2021-01-27 10:38:32 -0500

    following along with teacher, added prompt msg

--- GC_Password_Exercise/Program.cs
+++ GC_Password_Exercise/Program.cs
@@ -9,7 +9,20 @@ namespace GC_Password_Exercise
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string password;
+            while (true)
+            {
+                Console.WriteLine("Please input a passwrod that satisfies the following rules: ");
+                Console.WriteLine("1) The password must contain at least 1 lowercase letter");
+                Console.WriteLine("2) Must contain at least 1 uppercase letter");
+                Console.WriteLine("3) At least one number");
+                Console.WriteLine("4) Min 7 characters and max 12 characters inclusive");
+                Console.WriteLine("5) Have one of the following special characters: ! @ # $ % ^ & *");
+
+                password = Console.ReadLine();
+            }
+
+
         }
     }
 }
